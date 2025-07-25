db37d8b220a4af27cd744738dc7a2bf936e3596b
fenet128t <fenet128t@gmail.com>
2025-07-25 23:52:46 +0300

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+
+Route::get('/', function () {
+  return Inertia::render('dashboard');
+});
+// user routes
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => [\App\Http\Middleware\AdminMiddleware::class]], function () {
+    // Admin-only routes here
+    Route::get('/admin/dashboard', 'AdminController@dashboard');
+});
+
+// end
+
+
+// end
