ae5b7b3074ff298e7a52e2102d82909fa124a903
fenet128t <fenet128t@gmail.com>
2025-07-27 22:18:22 +0300

    component added

--- resources/js/pages/user/Dashboard.vue
+++ resources/js/pages/user/Dashboard.vue
@@ -1,22 +1,42 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import UserLayouts from './Layouts/UserLayouts.vue'
+
+defineProps({
+    orders: Array
+})
 </script>
-
 <template>
-    <Head title="Dashboard" />
+    <UserLayouts>
 
-    <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
-        </template>
+        <div class="relative  max-w-screen-xl py-24 mx-auto overflow-x-auto">
+            <table v-show="order.order_items.length > 0" v-for="order in orders" :key="order.id"
+                class="w-full text-sm text-left text-gray-500 dark:text-gray-400 mb-5">
+                <thead class="text-xs text-gray-700 uppercase  dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Order ID # {{ order.id }}
+                        </th>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in!</div>
-                </div>
-            </div>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in order.order_items" :key="item.id"
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                           {{item.product.title}}
+                        </th>
+                        <td class="px-6 py-4">
+                           {{item.product.brand.name}}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{item.product.category.name}}
+                        </td>
+                        <td class="px-6 py-4">
+                            ${{item.product.price}}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-    </AuthenticatedLayout>
-</template>
+
+    </UserLayouts></template>
